22de132214545a4ed75a6b407daae3f9ccb91f55
stan <stanimir_ak@abv.bg>
2020-04-06 11:25:29 +0300

    feat - Skill, Post added

--- src/main/java/com/community/weare/Models/Post.java
+++ src/main/java/com/community/weare/Models/Post.java
@@ -1,24 +1,44 @@
 package com.community.weare.Models;
 
 import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Entity
 @Table(name = "posts_table")
 public class Post {
 
     @Id
-    @Column(name = "postId")
+    @Column(name = "post_Id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int postId;
 
-    @Column(name = "isPublic")
+    @Column(name = "is_Public")
     private boolean isPublic;
 
     @Column(name = "content")
     private String content;
 
+    @Column(name = "picture")
+    private String picture;
+
+    @ManyToOne()
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @Column(name = "likes")
+    private int likes = 0;
+
+    @Column(name = "date_time")
+    private String date;
+
     public Post() {
+        Date date = new Date();
+
+        this.date = formatter.format(date);
     }
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
 
     public int getPostId() {
         return postId;
@@ -44,5 +64,35 @@ public class Post {
         this.content = content;
     }
 
-    //postId	isPublic	content	picture	userId	int likes	date:hour
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- src/main/java/com/community/weare/Models/Skill.java
+++ src/main/java/com/community/weare/Models/Skill.java
@@ -1,4 +1,20 @@
 package com.community.weare.Models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "skills_table")
 public class Skill {
+
+    @Id
+    @Column(name = "skill_id")
+    private int skillId;
+
+    @Column(name = "skill")
+    private String skill;
+
+
 }
